07acae03c6eb7862db38d933bcf153cdb129e34e
ClDaly2904 <c.l.daly@hotmail.com>
2022-05-06 19:42:23 +0000

    Add main function and contacts menu function for initialization.

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# importing the python libraries
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -15,4 +16,34 @@ SHEET = GSPREAD_CLIENT.open('contact_book')
 contacts = SHEET.worksheet('contacts')
 data = contacts.get_all_values()
 
-print(data)
+
+def contacts_menu():
+    """
+    Menu to initialize when program first runs. User can view all
+    the different contact options and make a selection.
+    """
+    print("****************************************"
+          "***************************************")
+    print("\t\t\t\tCONTACT BOOK", flush=False)
+    print("****************************************"
+          "***************************************")
+    print("Welcome to your contacts book! "
+          "Please select a number from the options below:")
+    print("1. Add a new contact")
+    print("2. Remove an existing contact")
+    print("3. Delete all contacts")
+    print("4. Search for a contact")
+    print("5. Display all contacts")
+    print("6. Update existing contact")
+    print("7. Exit phonebook")
+    choice = int(input("Please enter your choice: "))
+
+
+def main():
+    """
+    Program to run all functions
+    """
+    contacts_menu()
+
+
+main()
